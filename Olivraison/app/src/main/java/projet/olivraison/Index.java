1d0480a007955ec675b21a9ce9f8a12e364a876e
dkonate <dotemin@gmail.com>
2017-01-17 15:28:37 +0100

    mise a jour de l'activite admin
    J’ai rajouté le menu, et aussi retravailler la vue admin

--- Olivraison/app/src/main/java/projet/olivraison/Index.java
+++ Olivraison/app/src/main/java/projet/olivraison/Index.java
@@ -75,6 +75,8 @@ public class Index extends AppCompatActivity
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
+
+
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
